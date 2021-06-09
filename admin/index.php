dedcc7c953a0d62e3946eebeb5bbdbd49525722f
nathanbanich <jannathanfirdaus1245@gmail.com>
2021-06-09 14:02:05 +0700

    merging

--- admin/index.php
+++ admin/index.php
@@ -2,5 +2,39 @@
 include "layout/head.php";
 include "layout/side.php";
 ?>
-<div id="kk"></div>
+
+<style>
+    .konten {
+        display: flex;
+    }
+
+    .box {
+        width: 99%;
+    }
+</style>
+
+<div class="konten">
+    <div class="visi">
+        <div class="box">
+            <div class="box-body">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus autem aperiam quas aut fugit, magnam eligendi dolore est ab. Nulla quis ducimus eligendi, aperiam minus nemo quasi voluptatibus ut doloribus.
+            </div>
+        </div>
+    </div>
+    <div class="misi">
+        <div class="box">
+            <div class="box-body">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus autem aperiam quas aut fugit, magnam eligendi dolore est ab. Nulla quis ducimus eligendi, aperiam minus nemo quasi voluptatibus ut doloribus.
+            </div>
+        </div>
+    </div>
+    <div class="tujuan">
+        <div class="box">
+            <div class="box-body">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus autem aperiam quas aut fugit, magnam eligendi dolore est ab. Nulla quis ducimus eligendi, aperiam minus nemo quasi voluptatibus ut doloribus.
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php include "layout/foot.php"; ?>
